80874d4d9515aaabb832b409dfa2bc2a605203d4
Jacob <jacobhlnhtxb16@gmail.com>
2018-11-05 14:19:55 +0100

    prøvet at få text over npcer til at virker
    prøvet at få text over npcer til at virker, please få det til at virker statinære jacob

---
+++ Paint Game/Assets/TextSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextSpawner : MonoBehaviour
+{
+
+    public GameObject TextPrefab;
+    private Image Bar;
+
+    // Use this for initialization
+    void Start()
+    {
+        Bar = Instantiate(TextPrefab, FindObjectOfType<Canvas>().transform).GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Bar.transform.position = Camera.main.WorldToScreenPoint(transform.position);
+    }
+}

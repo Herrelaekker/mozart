9248ec0746d9e7ae7a1dbcd9c91d7365a394c2a7
Herrelaekker <40020645+Herrelaekker@users.noreply.github.com>
2018-11-18 21:17:37 +0100

    militærtøs anim + script som ikke virker

---
+++ Paint Game/Assets/MilitaryGirl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MilitaryGirl : MonoBehaviour {
+
+    public int speed;
+
+    private bool moving;
+
+	// Use this for initialization
+	void Start () {
+        moving = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (moving)
+        transform.Translate(-speed * Time.deltaTime, 0, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Trigger")
+        {
+            moving = false;
+            print("HAHA");
+        }
+        print("øv");
+    }
+
+}
